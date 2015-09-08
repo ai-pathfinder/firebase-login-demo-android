bf69ff72ea4af57b55436a2c1e51be79bce29d2e
Ralph Yozzo <ralph@brooklynmarathon.com>
2015-09-08 16:42:05 -0400

    format

--- app/src/main/java/com/brooklynmarathon/pathfinder/login/FirebaseListAdapter.java
+++ app/src/main/java/com/brooklynmarathon/pathfinder/login/FirebaseListAdapter.java
@@ -45,7 +45,7 @@ public abstract class FirebaseListAdapter<T> extends BaseAdapter {
      * @param mRef        The Firebase location to watch for data changes. Can also be a slice of a location, using some
      *                    combination of <code>limit()</code>, <code>startAt()</code>, and <code>endAt()</code>,
      * @param mModelClass Firebase will marshall the data at a location into an instance of a class that you provide
-     * @param mLayout     This is the mLayout used to represent a single list item. You will be responsible for populating an
+     * @param mLayout     This is the mLayout item. You will be responsible for populating an
      *                    instance of the corresponding view with the data from an instance of mModelClass.
      * @param activity    The activity containing the ListView
      */
